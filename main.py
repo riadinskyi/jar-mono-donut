1a3b5f514ba7feabfeae8ed6da038644cd26510d
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-08-31 14:53:42 +0200

    Enable CORS middleware for API.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from api_v1 import router
-app = FastAPI(title="Backend for serve and managing Jars by Monobank", version="0.1",)
+
+app = FastAPI(
+    title="Backend for serve and managing Jars by Monobank",
+    version="0.1",
+)
+
+# Дозволити CORS для всього API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 if __name__ == "__main__":
