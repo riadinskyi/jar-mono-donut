243c0c38f5b1e900651657e1c8d5c332c8d0dbf6
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-09-12 10:54:27 +0200

    Add user_name uniqueness check and update admin creation logic

--- api_v1/system/dependencies.py
+++ api_v1/system/dependencies.py
@@ -1,6 +1,10 @@
 import datetime
-
 import requests
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.admin import Admin
 
 
 async def request_info_about_client(token: str):
@@ -9,6 +13,17 @@ async def request_info_about_client(token: str):
     ).json()
     print("MONO API CALL")
     return api
+
+
+async def check_user_name_availability(user_name: str, session: AsyncSession) -> bool:
+    """
+    Перевірити, чи існує user_name, якщо так -> True
+    """
+    stmt = select(Admin).where(Admin.user_name == user_name)
+    result = await session.execute(stmt)
+    if result.scalars().first():
+        return True
+    return False
 
 
 def get_one_month_ago() -> int:
