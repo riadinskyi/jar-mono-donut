0015efa5152e73d6ebe5490889d3f802f639a7cd
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-09-26 15:36:01 +0200

    Initialize the table for permission

--- api_v1/system/schemas.py
+++ api_v1/system/schemas.py
@@ -1,4 +1,21 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class AdminPermission(str, Enum):
+    read_order = "read_order"
+    create_order = "create_order"
+    edit_order = "edit_order"
+    delete_order = "delete_order"
+
+    read_admin = "read_admin"
+    issue_new_admin = "issue_new_admin"
+    edit_admin = "edit_admin"
+    delete_admin = "delete_admin"
+
+    issue_new_permission = "issue_new_permission"
+    edit_permission = "edit_permission"
+    revoked_permission = "revoked_permission"
 
 
 class AdminBase(BaseModel):
