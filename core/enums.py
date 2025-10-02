8626f5877e5cb23fca8ccba911dbbe8f3068bbe1
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-10-02 14:16:01 +0200

    Extract `AdminPermission` enum to a dedicated `core.enums` module; update all references accordingly.

---
+++ core/enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class AdminPermission(str, Enum):
+    read_order = "read_order"
+    create_order = "create_order"
+    edit_order = "edit_order"
+    delete_order = "delete_order"
+
+    read_admin = "read_admin"
+    issue_new_admin = "issue_new_admin"
+    edit_admin = "edit_admin"
+    delete_admin = "delete_admin"
+
+    issue_new_permission = "issue_new_permission"
+    read_other_permission = "read_other_permission"
+    edit_permission = "edit_permission"
+    revoked_permission = "revoked_permission"
