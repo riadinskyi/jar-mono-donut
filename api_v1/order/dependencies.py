9632879358030bbe779dba21ebb98e7ba085eb4e
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-10-04 17:48:30 +0200

    Validate service tokens

--- api_v1/order/dependencies.py
+++ api_v1/order/dependencies.py
@@ -3,8 +3,17 @@ from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.config import service_token
 from core.models.order import Order, OrderStatus
 from core.models.payment import Payment
+
+
+async def check_service_token(token: str) -> bool:
+    if token != service_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+    return True
 
 
 async def return_order_by_id(order_id: int, session: AsyncSession) -> Order:
